075561b771c4b1c7c12d3952069117e96482c2c0
shroukshams <shroukshams237@gmail.com>
2026-07-13 23:52:45 +0300

    Fix: Format Terraform main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region = "us-east-1"
-  access_key = "test"
-  secret_key = "test"
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
   skip_credentials_validation = true
   endpoints {
     ec2 = "http://localhost:4566"
